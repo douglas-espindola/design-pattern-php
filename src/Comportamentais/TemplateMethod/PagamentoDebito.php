cd6d5b41490f105bed6e03c48dd0b8a3cd8415cf
Douglas Espindola <douglas.spindola15@gmail.com>
2024-01-04 22:50:30 -0300

    feat: implementado classes servicos pagamento

--- src/Comportamentais/TemplateMethod/PagamentoDebito.php
+++ src/Comportamentais/TemplateMethod/PagamentoDebito.php
@@ -2,34 +2,16 @@
 
 namespace DesignPatterns\Comportamentais\TemplateMethod;
 
-class PagamentoDebito
+class PagamentoDebito extends Pagamento
 {
-    private float $valor;
-    private Gateway $gateway;
-
-    public function __construct($valor, Gateway $gateway)
-    {
-        $this->valor = $valor;
-        $this->gateway = $gateway;
-    }
-
     //Calcula a taxa do Gateway.
     public function calcularTaxa(): float
     {
         //Retorna uma taxa de 4 reais.
         return 4;
     }
-
-    //Calcula o desconto.
     public function calcularDesconto(): float
     {
-        //Retorna o valor do pagamento com desconto de 5%.
-        return $this->valor * 0.05;
-    }
-
-    public function realizaCobranca(): bool
-    {
-        $valorFinal = $this->valor + $this->calcularTaxa() - $this->calcularDesconto();
-        return $this->gateway->cobrar($valorFinal);
+        return $this->getValor() * 0.05;
     }
 }
