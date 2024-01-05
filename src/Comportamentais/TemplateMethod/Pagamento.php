d7211ae68e02f445501d4c57bdcfd933950ac5a0
Douglas Espindola <douglas.spindola15@gmail.com>
2024-01-04 22:50:20 -0300

    feat: implementado classe template method

--- src/Comportamentais/TemplateMethod/Pagamento.php
+++ src/Comportamentais/TemplateMethod/Pagamento.php
@@ -2,7 +2,37 @@
 
     namespace DesignPatterns\Comportamentais\TemplateMethod;
 
-    class Pagamento
+    abstract class Pagamento
     {
+        private float $valor;
+        private Gateway $gateway;
 
+        public function __construct($valor, Gateway $gateway)
+        {
+            $this->valor   = $valor;
+            $this->gateway = $gateway;
+        }
+
+        public function getValor(): float
+        {
+            return $this->valor;
+        }
+
+        public function setValor(float $valor): void
+        {
+            $this->valor = $valor;
+        }
+
+        public function calcularTaxa(): float
+        {
+            return 0;
+        }
+
+        abstract public function calcularDesconto(): float;
+
+        public function realizaCobranca(): bool
+        {
+            $valorFinal = $this->valor + $this->calcularTaxa() - $this->calcularDesconto();
+            return $this->gateway->cobrar($valorFinal);
+        }
     }
