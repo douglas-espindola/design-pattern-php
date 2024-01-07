30549b0c9eac4318ea86a23fc7cc5984cbedacd6
Douglas Espindola <douglas.spindola15@gmail.com>
2024-01-06 22:50:32 -0300

    feat: implementado subject

--- src/Comportamentais/Observer/Newsletter.php
+++ src/Comportamentais/Observer/Newsletter.php
@@ -2,7 +2,34 @@
 
     namespace DesignPatterns\Comportamentais\Observer;
 
-    class Newsletter
+    class Newsletter implements Subject
     {
+        protected array $observers;
+        protected array $mensagem;
+        public function registerObserver(Observer $observer): void
+        {
+            $this->observers[] = $observer;
+        }
 
+        public function removeObserver(Observer $observer): void
+        {
+            foreach ($observer as $key => $observerInArray){
+                if($observerInArray === $observer){
+                    unset($this->observers[$key]);
+                }
+            }
+        }
+
+        public function notifyObservers(): void
+        {
+            foreach ($this->observers as $observer){
+                $observer->update(end($this->mensagem));
+            }
+        }
+
+        public function addMensagem(string $mensagem): void
+        {
+            $this->mensagem[] = $mensagem;
+            $this->notifyObservers();
+        }
     }
