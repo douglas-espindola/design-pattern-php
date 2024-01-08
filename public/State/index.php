241ed3044413537a5975267cdd86455cd43ee3df
Douglas Espindola <douglas.spindola15@gmail.com>
2024-01-07 22:50:34 -0300

    feat: implementado class cliente para teste

--- public/State/index.php
+++ public/State/index.php
@@ -1,3 +1,14 @@
 <?php
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+    use DesignPatterns\Comportamentais\State\Pedido;
+
+    require_once __DIR__ . '/../../vendor/autoload.php';
+
+    $pedido = new Pedido();
+    try {
+        $pedido->sucessoAoPagar();
+        $pedido->cancelarPedido();
+        $pedido->despacharPedido();
+    } catch (\Exception $e) {
+        echo $e->getMessage();
+    }
